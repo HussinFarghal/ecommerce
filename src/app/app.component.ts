c095697b64a736fdd5eef66bf53910321969b417
Hussein Hassan <hussein.hassan@vodafone.com.eg>
2023-05-28 22:48:15 +0300

    tsConfig

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'ecommerace';
+  public gifts: [] = [];
 
-  ngOnInit(): void {
-    this.fun();
+  constructor() {
+    // @ts-ignore
+    this.gifts.push({
+      id: '1',
+      name: 'Gift 1',
+      price: '100',
+    });
   }
 
-  fun = () => console.log('test');
+  ngOnInit(): void {
+    this.generateRandomOTP();
+    this.findObjectByKey(this.gifts, 'id', '1');
+  }
+
+  generateRandomOTP() {
+    // min number 6 digits, max number 7 digits - 1000000
+    return Math.floor(100000 + Math.random() * 900000);
+  }
+
+  findObjectByKey(array: string[], key, value: string) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i][key] === value) {
+        console.log(array[i]);
+        return array[i];
+      }
+    }
+    return null;
+  }
 }
