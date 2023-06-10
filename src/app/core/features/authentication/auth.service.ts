7812ed8cd064bc3e90038bccf7ee3f5c5415eb68
Hussein Hassan <hussein.hassan@vodafone.com.eg>
2023-06-10 18:02:08 +0300

    add auth func in authentication

--- src/app/core/features/authentication/auth.service.ts
+++ src/app/core/features/authentication/auth.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserToken } from '../../models/user-token';
+import { HttpClient } from '@angular/common/http';
+import { API_CONFIG } from 'src/configs/api.config';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
+  constructor(private _http: HttpClient) {}
+
+  public login(email: string, password: string): Observable<UserToken> {
+    return this._http
+      .post<UserToken>(API_CONFIG.login.url(email, password), {
+        email,
+        password,
+      })
+      .pipe((res) => res);
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.example.com',
+  authAPIUrl: 'https://api.escuelajs.co/api/v1',
+};
